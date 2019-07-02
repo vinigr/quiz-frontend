e71eaa1c477d5829ad69b1328c92b13ac5901a5e
Vinícios Grama <vinyirun1@gmail.com>
2019-07-02 14:26:25 -0300

    Início do projeto

---
+++ src/pages/Dashboard/index.js
@@ -0,0 +1,7 @@
+import React from "react";
+import { BrowserRouter, Switch, Route } from "react-router-dom";
+// import { Home } from "./pages";
+
+const Dashboard = () => <Switch></Switch>;
+
+export default Dashboard;
---
+++ src/pages/index.js
@@ -0,0 +1,6 @@
+import Home from "./Home";
+import Dashboard from "./Dashboard";
+import Login from "./Login";
+import Register from "./Register";
+
+export { Home, Login, Register, Dashboard };
