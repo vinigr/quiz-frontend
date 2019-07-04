5e33e0254c1cda12cc32f7ed7bec53516390bc32
Vinícios Grama <vinyirun1@gmail.com>
2019-07-03 23:50:36 -0300

    SideMenu iniciado

--- src/components/SideMenu/index.js
+++ src/components/SideMenu/index.js
@@ -1,16 +1,48 @@
 import React from "react";
-import { Menu, AppLogo, List, ItemList } from "./styles";
-import { Link } from "react-router-dom";
+import {
+  Menu,
+  AppLogo,
+  List,
+  ItemList,
+  LinkMenu,
+  IconHome,
+  IconSubject,
+  IconQuestion,
+  IconSettings,
+  Linha
+} from "./styles";
 import logo from "../../assets/img/logo-verde.png";
 
-export default function SideMenu() {
+export default function SideMenu(props) {
   return (
     <Menu>
       <AppLogo src={logo} />
       <List>
-        <ItemList>
-          <Link>
-        </ItemList>
+        <LinkMenu exact={true} to="/teacher">
+          <ItemList>
+            <IconHome />
+            Início
+          </ItemList>
+        </LinkMenu>
+        <LinkMenu to="/teacher/disciplinas">
+          <ItemList>
+            <IconSubject />
+            Disciplinas
+          </ItemList>
+        </LinkMenu>
+        <LinkMenu to="/teacher/questionarios">
+          <ItemList>
+            <IconQuestion />
+            Questionários
+          </ItemList>
+        </LinkMenu>
+        <Linha />
+        <LinkMenu>
+          <ItemList>
+            <IconSettings />
+            Configurações
+          </ItemList>
+        </LinkMenu>
       </List>
     </Menu>
   );
--- src/components/SideMenu/styles.js
+++ src/components/SideMenu/styles.js
@@ -1,19 +1,91 @@
-import styled, { keyframes } from "styled-components";
+import styled from "styled-components";
+import { NavLink } from "react-router-dom";
+import { Home } from "styled-icons/typicons/Home";
+import { Subject } from "styled-icons/material/Subject";
+import { QuestionAnswer } from "styled-icons/material/QuestionAnswer";
+import { Settings } from "styled-icons/material/Settings";
 
 export const Menu = styled.nav`
   background-color: #fff;
   display: flex;
   height: 100vh;
   width: 24%;
+  flex-direction: column;
 `;
 
 export const AppLogo = styled.img`
   height: 50px;
+  width: 50px;
 `;
 
 export const List = styled.ul`
   display: flex;
   flex-direction: column;
+  list-style: none;
+  padding: 0;
 `;
 
-export const ItemList = styled.li``;
+export const ItemList = styled.li`
+  display: flex;
+  align-items: center;
+`;
+
+export const LinkMenu = styled(NavLink)`
+  color: #000;
+  text-decoration: none;
+  font-family: "Rubik", sans-serif;
+  font-weight: 500;
+  display: flex;
+  align-items: center;
+  padding: 4px 10px 4px 24px;
+  margin-bottom: 8px;
+  height: 40px;
+  cursor: pointer;
+  &:hover {
+    color: #118d19;
+  }
+
+  &[aria-current] {
+    color: #015407;
+    background-color: #8ebe91;
+    border-right: 3px solid #015407;
+  }
+`;
+
+const colorIcon = `
+  color: #BFBFBF;
+  height: 30px;
+  margin-right: 10px;
+
+  ${LinkMenu}:hover &{
+    color: #118D19;
+  }
+
+  ${LinkMenu}[aria-current] &{
+    color: #015407;
+  }
+`;
+
+export const IconHome = styled(Home)`
+  ${colorIcon};
+`;
+
+export const IconSubject = styled(Subject)`
+  ${colorIcon};
+`;
+
+export const IconQuestion = styled(QuestionAnswer)`
+  ${colorIcon};
+`;
+
+export const IconSettings = styled(Settings)`
+  ${colorIcon};
+`;
+
+export const Linha = styled.hr`
+  border-width: 0;
+  height: 0.5px;
+  border-top-width: 1px;
+  color: #b2babb;
+  width: 100%;
+`;
