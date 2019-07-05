f6c4d8b6cc09d35db52a4bde07a7dfef6f7546c8
Vinícios Grama <vinyirun1@gmail.com>
2019-07-05 18:11:48 -0300

    Lista de disciplinas do professor

--- src/pages/Teacher/Disciplinas/index.js
+++ src/pages/Teacher/Disciplinas/index.js
@@ -1,6 +1,28 @@
-import React from "react";
-// import { Container } from "./styles";
+import React, { useState, useEffect } from "react";
+import { Container, ListaDisciplinas } from "./styles";
+import api from "../../../service/api";
+import DisciplinaItem from "../../../components/DisciplinaItem";
 
 export default function Disciplinas() {
-  return <div></div>;
+  const [subjects, setSubjects] = useState([]);
+
+  useEffect(() => {
+    buscaBanco();
+  }, []);
+
+  async function buscaBanco() {
+    const token = await localStorage.getItem("x-access-token");
+    const subject = await api.get("/teacher/subjects", {
+      headers: {
+        "x-access-token": token
+      }
+    });
+    await setSubjects(subject.data.subjects);
+  }
+
+  function renderDisciplinas() {
+    return <ListaDisciplinas>{subjects.map(DisciplinaItem)}</ListaDisciplinas>;
+  }
+
+  return <div>{renderDisciplinas()}</div>;
 }
---
+++ src/pages/Teacher/Disciplinas/styles.js
@@ -0,0 +1,9 @@
+import styled from "styled-components";
+
+export const Container = styled.div``;
+
+export const ListaDisciplinas = styled.ul`
+  list-style: none;
+  display: flex;
+  flex-wrap: wrap;
+`;
