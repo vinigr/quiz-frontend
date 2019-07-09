ca7a3270960593af7039e49d15bed00efd8cb29c
Vinícios Grama <vinyirun1@gmail.com>
2019-07-08 22:35:51 -0300

    Personalização header

--- src/components/HeaderMenu/index.js
+++ src/components/HeaderMenu/index.js
@@ -1,11 +1,40 @@
-import React from "react";
-import { Header, IconMenu, IconNotificacoes } from "./styles";
+import React, { useState, useEffect } from "react";
+import {
+  Header,
+  IconMenu,
+  OpcoesDisciplina,
+  OpcaoDisciplina,
+  NomeOpcao,
+  IconNotificacoes
+} from "./styles";
 
-export default function HeaderMenu() {
+export default function HeaderMenu(props) {
+  const [location, setLocation] = useState([]);
+
+  useEffect(() => {
+    const { pathname } = props.location;
+    setLocation(pathname.split(/\//));
+  }, [props]);
+
   return (
     <Header>
-      <IconMenu />
+      <IconMenu onClick={() => props.setSideMenu(!props.sideMenu)} />
+      {(location[2] === "d") & !isNaN(location[3]) ? (
+        <OpcoesDisciplina>
+          <OpcaoDisciplina to={`/t/d/${location[3]}/m`}>
+            <NomeOpcao>Mural</NomeOpcao>
+          </OpcaoDisciplina>
+          <OpcaoDisciplina to={`/t/d/${location[3]}/p`}>
+            <NomeOpcao>Pessoas</NomeOpcao>
+          </OpcaoDisciplina>
+        </OpcoesDisciplina>
+      ) : (
+        <></>
+      )}
+
       <IconNotificacoes />
     </Header>
   );
 }
+
+// export default withRouter(HeaderMenu);
--- src/components/HeaderMenu/styles.js
+++ src/components/HeaderMenu/styles.js
@@ -1,11 +1,11 @@
 import styled from "styled-components";
+import { NavLink } from "react-router-dom";
 import { NotificationsNone } from "styled-icons/material/NotificationsNone";
 import { Menu } from "styled-icons/material/Menu";
 
 export const Header = styled.header`
   height: 60px;
   background-color: #fff;
-  /* width: 76%; */
   padding: 0 20px;
   display: flex;
   justify-content: space-between;
@@ -25,6 +25,36 @@ export const IconNotificacoes = styled(NotificationsNone)`
   height: 30px;
 
   ${Icon};
+`;
+
+export const OpcoesDisciplina = styled.div`
+  height: 100%;
+  display: flex;
+  align-items: center;
+`;
+
+export const OpcaoDisciplina = styled(NavLink)`
+  padding: 0 16px;
+  height: 100%;
+  color: #000;
+  text-decoration: none;
+  display: flex;
+  align-items: center;
+  &:hover {
+    background-color: #dbdbdb;
+  }
+
+  &[aria-current] {
+    background-color: #8ebe91;
+    border-bottom: 3px solid #015407;
+  }
+`;
+
+export const NomeOpcao = styled.h2`
+  font-family: "Rubik", sans-serif;
+  font-weight: 500;
+  font-size: 14px;
+  margin: 0;
 `;
 
 export const IconMenu = styled(Menu)`
