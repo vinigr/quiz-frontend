ee1d42d78a94bb5f5e820bd2a0358b0bdd5f13ab
Vinícios Grama <vinyirun1@gmail.com>
2019-07-08 22:38:39 -0300

    Início de personalização para side menu

--- src/pages/Teacher/index.js
+++ src/pages/Teacher/index.js
@@ -1,24 +1,32 @@
-import React from "react";
+import React, { useState } from "react";
 import { Container, Main, Content } from "./styles";
 import HeaderMenu from "../../components/HeaderMenu";
 import SideMenu from "../../components/SideMenu";
 import Home from "./Home";
 import Disciplinas from "./Disciplinas";
+import Disciplina from "./Disciplina";
 import { Switch, Route } from "react-router-dom";
 
 export default function Teacher(props) {
+  const [sideMenu, setSideMenu] = useState(true);
+
   return (
     <Container>
-      <SideMenu />
+      <SideMenu sideMenu={sideMenu} setSideMenu={setSideMenu} />
       <Content>
-        <HeaderMenu />
+        <HeaderMenu {...props} sideMenu={sideMenu} setSideMenu={setSideMenu} />
         <Main>
           <Switch>
             <Route exact path={`${props.match.path}/`} component={Home} />
             <Route
               exact
-              path={`${props.match.path}/disciplinas`}
+              path={`${props.match.path}/d`}
               component={Disciplinas}
+            />
+            <Route
+              exact
+              path={`${props.match.path}/d/:id`}
+              component={Disciplina}
             />
           </Switch>
         </Main>
