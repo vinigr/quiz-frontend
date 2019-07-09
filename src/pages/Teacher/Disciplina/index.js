29ab48d1540c57c17485047f9a4eb3278a2fef00
Vinícios Grama <vinyirun1@gmail.com>
2019-07-09 16:03:46 -0300

    Padding adicionado na tela da disciplina

--- src/pages/Teacher/Disciplina/index.js
+++ src/pages/Teacher/Disciplina/index.js
@@ -2,13 +2,15 @@ import React from "react";
 import { Switch, Route } from "react-router-dom";
 import Pessoas from "./Pessoas";
 import Mural from "./Mural";
-// import { Container } from './styles';
+import { Container } from "./styles";
 
 export default function Disciplina(props) {
   return (
-    <Switch>
-      <Route exact path={`${props.match.path}/`} component={Mural} />
-      <Route path={`${props.match.path}/p`} component={Pessoas} />
-    </Switch>
+    <Container>
+      <Switch>
+        <Route exact path={`${props.match.path}/`} component={Mural} />
+        <Route path={`${props.match.path}/p`} component={Pessoas} />
+      </Switch>
+    </Container>
   );
 }
