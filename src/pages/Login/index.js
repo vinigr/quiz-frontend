f4835c127bdaad11ca4232b5969f31ec23e49386
Vinícios Grama <vinyirun1@gmail.com>
2019-07-14 22:52:19 -0300

    Página de Login adiantada

--- src/pages/Login/index.js
+++ src/pages/Login/index.js
@@ -2,6 +2,11 @@ import React, { useEffect } from "react";
 
 import {
   Container,
+  Header,
+  LinkTelaInicial,
+  AppLogo,
+  NomeSite,
+  Conteudo,
   SectionForm,
   Title,
   DivIcons,
@@ -10,8 +15,15 @@ import {
   IconGoogle,
   TextForm,
   Form,
-  Input
+  Input,
+  LinkRecuperacao,
+  BotaoEntrar,
+  DivInfo,
+  TitleInfo,
+  TextInfo,
+  LinkCadastro
 } from "./styles";
+import logo from "../../assets/img/logo-verde.png";
 
 export default function Login() {
   useEffect(() => {
@@ -20,21 +32,39 @@ export default function Login() {
 
   return (
     <Container>
-      <SectionForm>
-        <Title>Entre</Title>
-        <DivIcons>
-          <ButtonSocial>
-            <IconFacebook />
-          </ButtonSocial>
-          <ButtonSocial>
-            <IconGoogle />
-          </ButtonSocial>
-        </DivIcons>
-        <TextForm>ou use sua conta de email</TextForm>
-        <Form>
-          <Input type="email" placeholder="Email" />
-        </Form>
-      </SectionForm>
+      <Header>
+        <LinkTelaInicial to="/">
+          <AppLogo src={logo} />
+          <NomeSite>Quest On</NomeSite>
+        </LinkTelaInicial>
+      </Header>
+      <Conteudo>
+        <SectionForm>
+          <Title>Entre</Title>
+          <DivIcons>
+            <ButtonSocial rede="facebook">
+              <IconFacebook />
+            </ButtonSocial>
+            <ButtonSocial>
+              <IconGoogle />
+            </ButtonSocial>
+          </DivIcons>
+          <TextForm>ou use sua conta de email</TextForm>
+          <Form>
+            <Input type="email" placeholder="Email" />
+            <Input type="password" placeholder="Password" />
+            <LinkRecuperacao to="forgot-password">
+              Esqueceu sua senha?
+            </LinkRecuperacao>
+            <BotaoEntrar>Entrar</BotaoEntrar>
+          </Form>
+        </SectionForm>
+        <DivInfo>
+          <TitleInfo>Olá, amigo(a)</TitleInfo>
+          <TextInfo>Ainda não tem uma conta em nossa plataforma?</TextInfo>
+          <LinkCadastro to="register">Cadastre-se</LinkCadastro>
+        </DivInfo>
+      </Conteudo>
     </Container>
   );
 }
--- src/pages/Login/styles.js
+++ src/pages/Login/styles.js
@@ -1,6 +1,7 @@
 import styled from "styled-components";
 import { FacebookF } from "styled-icons/fa-brands/FacebookF";
 import { Google } from "styled-icons/fa-brands/Google";
+import { Link } from "react-router-dom";
 
 export const Container = styled.div`
   width: 100vw;
@@ -8,16 +9,53 @@ export const Container = styled.div`
   font-family: "Rubik", sans-serif;
 `;
 
+export const Header = styled.header`
+  display: flex;
+  align-items: center;
+  padding: 5px 20px;
+  justify-content: flex-start;
+  width: calc(100vw - 40px);
+  position: absolute;
+  top: 0;
+  left: 0;
+  background-color: transparent;
+`;
+
+export const LinkTelaInicial = styled(Link)`
+  text-decoration: none;
+  display: flex;
+  align-items: center;
+  color: #000;
+`;
+
+export const AppLogo = styled.img`
+  height: 50px;
+  width: 50px;
+`;
+
+export const NomeSite = styled.h1`
+  font-family: "Rubik", sans-serif;
+  font-weight: 500;
+  margin: 0;
+  font-size: 20px;
+`;
+
+export const Conteudo = styled.main`
+  display: flex;
+  height: 100vh;
+`;
+
 export const SectionForm = styled.section`
   width: 60vw;
-  height: 100%;
   display: flex;
   flex-direction: column;
   align-items: center;
+  padding: 40px;
 `;
 
 export const Title = styled.h2`
   font-size: 30px;
+  color: #059451;
 `;
 
 export const DivIcons = styled.div`
@@ -34,16 +72,30 @@ export const ButtonSocial = styled.button`
   width: 60px;
   justify-content: center;
   margin: 0 10px;
+  cursor: pointer;
+
+  &:hover {
+    background-color: #${props => (props.rede === "facebook" ? "3b5998" : "ea4335")};
+    border: none;
+  }
 `;
 
 export const IconFacebook = styled(FacebookF)`
   height: 30px;
   color: #000;
+
+  ${ButtonSocial}:hover & {
+    color: #fff;
+  }
 `;
 
 export const IconGoogle = styled(Google)`
   height: 30px;
   color: #000;
+
+  ${ButtonSocial}:hover & {
+    color: #fff;
+  }
 `;
 
 export const TextForm = styled.h4`
@@ -59,10 +111,67 @@ export const Form = styled.form`
 `;
 
 export const Input = styled.input`
-  background-color: #e5e5e5;
+  background-color: #f1f1f1;
   border: none;
   border-radius: 5px;
   height: 40px;
   padding: 4px 6px;
-  width: 60%;
+  width: 80%;
+  margin-bottom: 14px;
+`;
+
+export const LinkRecuperacao = styled(Link)`
+  font-size: 16px;
+  font-weight: 600px;
+  margin-top: 20px;
+  margin-bottom: 30px;
+  color: #434343;
+`;
+
+export const BotaoEntrar = styled.button`
+  background-color: #059451;
+  border: none;
+  border-radius: 5px;
+  height: 40px;
+  padding: 4px 6px;
+  width: 30%;
+  color: #f1f1f1;
+  font-size: 24px;
+  font-family: "Rubik", sans-serif;
+  cursor: pointer;
+`;
+
+export const DivInfo = styled.div`
+  background-color: #059451;
+  height: calc(100vh - ${Header});
+  width: 40vw;
+  padding: 40px;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  color: #fff;
+`;
+
+export const TitleInfo = styled.h2`
+  font-weight: 600;
+  font-size: 36px;
+  margin: 0 0 5px;
+`;
+
+export const TextInfo = styled.h3`
+  font-weight: 500;
+  font-size: 24px;
+  text-align: center;
+`;
+
+export const LinkCadastro = styled(Link)`
+  font-size: 16px;
+  font-weight: 400;
+  border-radius: 30px;
+  padding: 18px;
+  color: #fff;
+  border: solid 1px #fff;
+  text-decoration: none;
+  text-transform: uppercase;
 `;
