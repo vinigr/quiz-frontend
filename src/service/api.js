1fe3e5f3a9c6d64dcfb88a2a4255632e1654c052
Vinícios Grama <vinyirun1@gmail.com>
2019-07-15 22:40:56 -0300

    token adicionado ao cabeçalho das requisições

--- src/service/api.js
+++ src/service/api.js
@@ -1,7 +1,16 @@
 import axios from "axios";
+import AuthService from "./auth";
 
 const api = axios.create({
   baseURL: "http://192.168.0.116:3110"
+});
+
+api.interceptors.request.use(async config => {
+  const token = AuthService.getToken();
+  if (token) {
+    config.headers.authorization = token;
+  }
+  return config;
 });
 
 export default api;
