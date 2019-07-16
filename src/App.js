a3da35a4ac8ed87415e6c6fa40406be33ed8d853
Vinícios Grama <vinyirun1@gmail.com>
2019-07-15 22:43:17 -0300

    Início da implementação de autorização de usuários as rotas

--- src/App.js
+++ src/App.js
@@ -1,6 +1,35 @@
 import React from "react";
-import { BrowserRouter, Switch, Route } from "react-router-dom";
+import { BrowserRouter, Switch, Route, Redirect } from "react-router-dom";
 import { Home, Login, Register, Player, Teacher } from "./pages";
+import AuthService from "./service/auth";
+
+const PlayerRoute = ({ component: Component, ...rest }) => (
+  <Route
+    {...rest}
+    x
+    render={props =>
+      AuthService.loggedIn() && AuthService.getRole() === 1 ? (
+        <Component {...props} />
+      ) : (
+        <Redirect to={{ pathname: "/", state: { from: props.location } }} />
+      )
+    }
+  />
+);
+
+const TeacherRoute = ({ component: Component, ...rest }) => (
+  <Route
+    {...rest}
+    x
+    render={props =>
+      AuthService.loggedIn() && AuthService.getRole() === 2 ? (
+        <Component {...props} />
+      ) : (
+        <Redirect to={{ pathname: "/", state: { from: props.location } }} />
+      )
+    }
+  />
+);
 
 const App = () => (
   <BrowserRouter>
@@ -8,8 +37,9 @@ const App = () => (
       <Route exact path="/" component={Home} />
       <Route path="/login" component={Login} />
       <Route path="/register" component={Register} />
-      <Route path="/p" component={Player} />
-      <Route path="/t" component={Teacher} />
+      <PlayerRoute path="/p" component={props => <Player {...props} />} />
+      <TeacherRoute path="/t" component={props => <Teacher {...props} />} />
+      <Route path="*" component={() => <h1>Page not found</h1>} />
     </Switch>
   </BrowserRouter>
 );
