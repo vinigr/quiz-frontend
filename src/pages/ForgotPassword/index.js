d09d2c0d150c5c5a3f4861932742334f884b810b
Vinícios Grama <vinyirun1@gmail.com>
2019-07-17 11:40:26 -0300

    Tela para envio de email de recuperação

--- src/pages/ForgotPassword/index.js
+++ src/pages/ForgotPassword/index.js
@@ -1,10 +1,57 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 
-import { Container, LinkTelaInicial, AppLogo, NomeSite } from "../Login/styles";
-import logo from "../../assets/img/logo-branca.png";
-import { Header } from "./styles";
+import {
+  Container,
+  Conteudo,
+  LinkTelaInicial,
+  AppLogo,
+  NomeSite,
+  IconMail,
+  Input,
+  TextError
+} from "../Login/styles";
+
+import {
+  Header,
+  Section,
+  Title,
+  Form,
+  DivEmail,
+  BotaoRecuperar,
+  TextSuccess
+} from "./styles";
+import logo from "../../assets/img/logo-verde.png";
+
+import api from "../../service/api";
 
 export default function ForgotPassword() {
+  const [email, setEmail] = useState("");
+  const [success, setSuccess] = useState(null);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    document.title = "Recuperação de senha";
+  }, []);
+
+  async function enviarEmail(e) {
+    e.preventDefault();
+    await setError(null);
+    await setSuccess(null);
+    if (!email || email === "") {
+      setError("Dados insuficientes!");
+      return;
+    }
+    try {
+      const msg = await api.put("/forgotPassword", {
+        email
+      });
+
+      setSuccess(msg.data.message);
+    } catch (err) {
+      return setError(err.response.data.message);
+    }
+  }
+
   return (
     <Container>
       <Header>
@@ -13,6 +60,28 @@ export default function ForgotPassword() {
           <NomeSite>Quest On</NomeSite>
         </LinkTelaInicial>
       </Header>
+      <Conteudo>
+        <Section>
+          <Title>Recupere sua senha</Title>
+          <Form>
+            <DivEmail>
+              <IconMail />
+              <Input
+                type="email"
+                value={email}
+                onChange={e => setEmail(e.target.value)}
+                placeholder="Email"
+                required
+              />
+            </DivEmail>
+            <BotaoRecuperar onClick={e => enviarEmail(e)}>
+              Enviar
+            </BotaoRecuperar>
+            {error && <TextError>{error}</TextError>}
+            {success && <TextSuccess>{success}</TextSuccess>}
+          </Form>
+        </Section>
+      </Conteudo>
     </Container>
   );
 }
--- src/pages/ForgotPassword/styles.js
+++ src/pages/ForgotPassword/styles.js
@@ -9,5 +9,67 @@ export const Header = styled.header`
   position: absolute;
   top: 0;
   left: 0;
+  background-color: transparent;
+`;
+
+export const Section = styled.section`
+  width: 100vw;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  padding: 40px;
+  justify-content: center;
+`;
+
+export const Title = styled.h2`
+  font-size: 32px;
+  color: #059451;
+`;
+
+export const Form = styled.form`
+  width: 40%;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+`;
+
+export const DivEmail = styled.div`
+  display: flex;
+  background-color: #f1f1f1;
+  width: 60%;
+  border-radius: 5px;
+  height: 40px;
+  margin-bottom: 14px;
+  align-items: center;
+  padding: 4px 6px;
+
+  &:focus-within {
+    border: 1px solid #151515;
+  }
+`;
+
+export const BotaoRecuperar = styled.button`
   background-color: #059451;
+  border: none;
+  border-radius: 5px;
+  height: 40px;
+  padding: 4px 6px;
+  width: 30%;
+  color: #f1f1f1;
+  font-size: 20px;
+  font-family: "Rubik", sans-serif;
+  cursor: pointer;
+  font-weight: 500;
+
+  &:hover {
+    opacity: 0.8;
+  }
+`;
+
+export const TextSuccess = styled.h4`
+  font-weight: 500;
+  font-size: 14px;
+  text-align: center;
+  color: #4bb543;
+  margin-top: 20px;
 `;
