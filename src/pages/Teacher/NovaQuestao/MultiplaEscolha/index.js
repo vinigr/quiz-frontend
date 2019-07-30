2b30fce8ee1817a6753b5d29b1ccd5472186f915
Vinícios Grama <vinyirun1@gmail.com>
2019-07-29 22:44:16 -0300

    Inicia a criação de questão de múltipla escolha

--- src/pages/Teacher/NovaQuestao/MultiplaEscolha/index.js
+++ src/pages/Teacher/NovaQuestao/MultiplaEscolha/index.js
@@ -1,20 +1,89 @@
-import React from "react";
+import React, { useState } from "react";
 
 import Dropzone from "react-dropzone";
-import { Container, DivUpload, IconImage, ButtonCreate } from "./styles";
+import {
+  Container,
+  DivUpload,
+  IconImage,
+  LabelOption,
+  InputRadio,
+  DivOptions,
+  DivOption,
+  AreaOption,
+  IconVerified,
+  ButtonCreate
+} from "./styles";
+
+const _defaultCosts = [
+  {
+    option: ""
+  },
+  {
+    option: ""
+  },
+  {
+    option: ""
+  },
+  {
+    option: ""
+  },
+  {
+    option: ""
+  }
+];
 
 export default function MultiplaEscolha() {
+  const [options, setOptions] = useState(_defaultCosts);
+  const [answerCorrect, setAnswerCorrect] = useState();
+
+  function handleOptionsChange(e) {
+    console.log(answerCorrect);
+    const _tempOptions = [...options];
+    _tempOptions[e.target.dataset.id][e.target.name] = e.target.value;
+
+    setOptions(_tempOptions);
+  }
+
   return (
     <Container>
       <Dropzone>
         {({ getRootProps, getInputProps }) => (
-          <DivUpload className="upload" {...getRootProps()}>
+          <DivUpload {...getRootProps()}>
             <IconImage />
             <input {...getInputProps()} />
             <p>Arraste uma imagem ou clique aqui</p>
           </DivUpload>
         )}
-      </Dropzone>      
+      </Dropzone>
+
+      <DivOptions>
+        {options.map((item, index) => (
+          <DivOption key={index}>
+            <InputRadio
+              name="option"
+              id={`option${index}`}
+              value={index}
+              type="radio"
+              onChange={e => {
+                setAnswerCorrect(e.target.value);
+              }}
+              hidden
+            />
+            <LabelOption htmlFor={`option${index}`}>
+              <AreaOption
+                name="option"
+                data-id={index}
+                placeholder={`Opção ${index + 1}`}
+                value={item.option}
+                rows="3"
+                onChange={handleOptionsChange}
+              />
+              <IconVerified />
+            </LabelOption>
+          </DivOption>
+        ))}
+      </DivOptions>
+
       <ButtonCreate>Cadastrar questão</ButtonCreate>
     </Container>
   );
--- src/pages/Teacher/NovaQuestao/MultiplaEscolha/styles.js
+++ src/pages/Teacher/NovaQuestao/MultiplaEscolha/styles.js
@@ -1,21 +1,66 @@
 import styled from "styled-components";
 import { ImageAdd } from "styled-icons/boxicons-regular/ImageAdd";
+import { CheckCircle } from "styled-icons/boxicons-solid/CheckCircle";
 
-export const Container = styled.div``;
+export const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  width: 80%;
+  align-items: center;
+`;
 
 export const DivUpload = styled.div`
   border-radius: 4px;
-  padding: 30px;
+  padding: 20px;
   text-align: center;
   border: 1px dashed #ddd;
   color: #999;
   margin-top: 50px;
   cursor: pointer;
+  width: 60%;
+  margin-bottom: 20px;
 `;
 
 export const IconImage = styled(ImageAdd)`
   color: #999;
   height: 60px;
+`;
+
+export const InputRadio = styled.input`
+  background-position: 0 -30px;
+`;
+
+export const LabelOption = styled.label`
+  display: flex;
+  align-items: center;
+  padding: 5px 10px;
+  justify-content: space-around;
+  border-radius: 5px;
+`;
+
+export const IconVerified = styled(CheckCircle)`
+  color: #fff;
+  height: 60px;
+`;
+
+export const DivOptions = styled.div`
+  display: flex;
+  flex-direction: column;
+  width: 60%;
+`;
+
+export const DivOption = styled.div`
+  & ${InputRadio}:checked + ${LabelOption} {
+    background-color: #37996b;
+  }
+`;
+
+export const AreaOption = styled.textarea`
+  resize: none;
+  width: 80%;
+  border-radius: 2px;
+  background-color: #f2f7ef;
+  padding: 5px;
 `;
 
 export const ButtonCreate = styled.button`
