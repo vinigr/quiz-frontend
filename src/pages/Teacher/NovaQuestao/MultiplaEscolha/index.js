afef4a47c1419bced3d03fd135e488064e1bf6d1
Vinícios Grama <vinyirun1@gmail.com>
2019-07-30 22:31:15 -0300

    Melhora processo de criação de pergunta

--- src/pages/Teacher/NovaQuestao/MultiplaEscolha/index.js
+++ src/pages/Teacher/NovaQuestao/MultiplaEscolha/index.js
@@ -1,9 +1,13 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 
-import Dropzone from "react-dropzone";
+import { useDropzone } from "react-dropzone";
 import {
   Container,
   DivUpload,
+  DivImage,
+  Image,
+  IconClose,
+  AreaPergunta,
   IconImage,
   LabelOption,
   InputRadio,
@@ -11,8 +15,11 @@ import {
   DivOption,
   AreaOption,
   IconVerified,
+  TextError,
   ButtonCreate
 } from "./styles";
+
+import api from "../../../../service/api";
 
 const _defaultCosts = [
   {
@@ -33,29 +40,90 @@ const _defaultCosts = [
 ];
 
 export default function MultiplaEscolha() {
+  const [question, setQuestion] = useState("");
+  const [image, setImage] = useState();
   const [options, setOptions] = useState(_defaultCosts);
   const [answerCorrect, setAnswerCorrect] = useState();
+  const [error, setError] = useState(null);
+
+  const { getRootProps, getInputProps } = useDropzone({
+    accept: "image/*",
+    onDropAccepted: acceptedFile => {
+      setImage(
+        Object.assign(acceptedFile[0], {
+          preview: URL.createObjectURL(acceptedFile[0])
+        })
+      );
+    }
+  });
 
   function handleOptionsChange(e) {
-    console.log(answerCorrect);
     const _tempOptions = [...options];
     _tempOptions[e.target.dataset.id][e.target.name] = e.target.value;
 
     setOptions(_tempOptions);
   }
 
+  async function registerQuestion(e) {
+    e.preventDefault();
+    setError(null);
+
+    if (!question) return setError("Pergunta incompleta!");
+    if (!answerCorrect) return setError("Opção correta não selecionada!");
+
+    const optionsValid = options.filter(option => {
+      return option.option !== "";
+    });
+
+    if (optionsValid.indexOf(options[answerCorrect]) === -1)
+      return setError("Opção marcada como correta não possui texto!");
+
+    if (optionsValid.length < 2)
+      return setError("É preciso ao menos 2 alternativas válidas");
+
+    const answer = optionsValid.indexOf(options[answerCorrect]);
+
+    const data = new FormData();
+
+    data.append("question", question);
+    data.append("image", image);
+    data.append("options", JSON.stringify(optionsValid));
+    data.append("answer", answer);
+
+    try {
+      const question = await api.post("/questionME", data);
+    } catch (err) {
+      return setError(err.data.message);
+    }
+  }
+
+  useEffect(() => {
+    if (image) {
+      URL.revokeObjectURL(image.preview);
+    }
+  }, [image]);
+
   return (
     <Container>
-      <Dropzone>
-        {({ getRootProps, getInputProps }) => (
-          <DivUpload {...getRootProps()}>
-            <IconImage />
-            <input {...getInputProps()} />
-            <p>Arraste uma imagem ou clique aqui</p>
-          </DivUpload>
-        )}
-      </Dropzone>
-
+      <AreaPergunta
+        value={question}
+        placeholder="Pergunta"
+        rows="5"
+        required
+        onChange={e => setQuestion(e.target.value)}
+      />
+      {image ? (
+        <DivImage>
+          <Image src={image.preview} />
+          <IconClose onClick={() => setImage(null)} />
+        </DivImage>
+      ) : (
+        <DivUpload {...getRootProps()}>
+          <IconImage />
+          <input {...getInputProps()} />
+          <p>Arraste uma imagem ou clique aqui</p>
+        </DivUpload>
+      )}
       <DivOptions>
         {options.map((item, index) => (
           <DivOption key={index}>
@@ -78,13 +146,13 @@ export default function MultiplaEscolha() {
                 rows="3"
                 onChange={handleOptionsChange}
               />
-              <IconVerified />
+              {index === parseInt(answerCorrect) && <IconVerified />}
             </LabelOption>
           </DivOption>
         ))}
       </DivOptions>
-
-      <ButtonCreate>Cadastrar questão</ButtonCreate>
+      {error && <TextError>{error}</TextError>}
+      <ButtonCreate onClick={registerQuestion}>Cadastrar questão</ButtonCreate>
     </Container>
   );
 }
--- src/pages/Teacher/NovaQuestao/MultiplaEscolha/styles.js
+++ src/pages/Teacher/NovaQuestao/MultiplaEscolha/styles.js
@@ -1,12 +1,14 @@
 import styled from "styled-components";
 import { ImageAdd } from "styled-icons/boxicons-regular/ImageAdd";
 import { CheckCircle } from "styled-icons/boxicons-solid/CheckCircle";
+import { Close } from "styled-icons/material/Close";
 
 export const Container = styled.div`
   display: flex;
   flex-direction: column;
   width: 80%;
   align-items: center;
+  margin-top: 50px;
 `;
 
 export const DivUpload = styled.div`
@@ -15,10 +17,44 @@ export const DivUpload = styled.div`
   text-align: center;
   border: 1px dashed #ddd;
   color: #999;
-  margin-top: 50px;
+  margin-top: 20px;
   cursor: pointer;
   width: 60%;
   margin-bottom: 20px;
+`;
+
+export const DivImage = styled.div`
+  border-radius: 4px;
+  padding: 20px;
+  text-align: center;
+  border: 1px dashed #ddd;
+  color: #999;
+  margin-top: 20px;
+  cursor: pointer;
+  width: 60%;
+  margin-bottom: 20px;
+  display: flex;
+  flex-direction: column;
+`;
+
+export const Image = styled.img`
+  width: 100%;
+`;
+
+export const IconClose = styled(Close)`
+  color: #999;
+  height: 30px;
+  /* position: absolute;
+  right: 5;
+  top: 5; */
+`;
+
+export const AreaPergunta = styled.textarea`
+  resize: none;
+  width: 80%;
+  border-radius: 2px;
+  background-color: #f2f7ef;
+  padding: 5px;
 `;
 
 export const IconImage = styled(ImageAdd)`
@@ -36,6 +72,9 @@ export const LabelOption = styled.label`
   padding: 5px 10px;
   justify-content: space-around;
   border-radius: 5px;
+  cursor: pointer;
+  background-color: #cdcdcd;
+  margin-bottom: 5px;
 `;
 
 export const IconVerified = styled(CheckCircle)`
@@ -47,6 +86,7 @@ export const DivOptions = styled.div`
   display: flex;
   flex-direction: column;
   width: 60%;
+  margin-bottom: 30px;
 `;
 
 export const DivOption = styled.div`
@@ -61,6 +101,15 @@ export const AreaOption = styled.textarea`
   border-radius: 2px;
   background-color: #f2f7ef;
   padding: 5px;
+`;
+
+export const TextError = styled.h4`
+  font-family: "Rubik", sans-serif;
+  font-weight: 500;
+  font-size: 16px;
+  text-align: center;
+  color: #d41919;
+  margin: 0 0 10px;
 `;
 
 export const ButtonCreate = styled.button`
