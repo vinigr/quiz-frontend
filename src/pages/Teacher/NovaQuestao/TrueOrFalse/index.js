429ffc369e16dfdfa2f583c17014367e952ec7cc
Vinícios Grama <vinyirun1@gmail.com>
2019-08-01 23:00:29 -0300

    Realiza o cadastro de questões verdadeiro e falso

--- src/pages/Teacher/NovaQuestao/TrueOrFalse/index.js
+++ src/pages/Teacher/NovaQuestao/TrueOrFalse/index.js
@@ -1,7 +1,122 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import { useDropzone } from "react-dropzone";
 
-// import { Container } from './styles';
+import {
+  Container,
+  DivOptions,
+  DivOption,
+  InputRadio,
+  LabelOption
+} from "./styles";
+import {
+  AreaPergunta,
+  DivImage,
+  Image,
+  IconClose,
+  DivUpload,
+  IconImage,
+  TextError,
+  ButtonCreate
+} from "../MultiplaEscolha/styles";
+
+import api from "../../../../service/api";
 
 export default function TrueOrFalse() {
-  return <div />;
+  const [question, setQuestion] = useState("");
+  const [image, setImage] = useState();
+  const [answerCorrect, setAnswerCorrect] = useState();
+  const [error, setError] = useState(null);
+
+  const { getRootProps, getInputProps } = useDropzone({
+    accept: "image/*",
+    onDropAccepted: acceptedFile => {
+      setImage(
+        Object.assign(acceptedFile[0], {
+          preview: URL.createObjectURL(acceptedFile[0])
+        })
+      );
+    }
+  });
+
+  useEffect(() => {
+    if (image) {
+      URL.revokeObjectURL(image.preview);
+    }
+  }, [image]);
+
+  async function registerQuestion(e) {
+    e.preventDefault();
+    setError(null);
+
+    if (!question) return setError("Pergunta incompleta!");
+    if (!answerCorrect) return setError("Opção correta não selecionada!");
+
+    const data = new FormData();
+
+    data.append("question", question);
+    data.append("image", image);
+    data.append("answer", answerCorrect);
+
+    try {
+      const resp = await api.post("/questionTf", data);
+      console.log(resp.data);
+    } catch (err) {
+      console.log(err);
+      // return setError(err.data.message);
+    }
+  }
+
+  return (
+    <Container>
+      <AreaPergunta
+        value={question}
+        placeholder="Pergunta"
+        rows="5"
+        required
+        onChange={e => setQuestion(e.target.value)}
+      />
+      {image ? (
+        <DivImage>
+          <Image src={image.preview} />
+          <IconClose onClick={() => setImage(null)} />
+        </DivImage>
+      ) : (
+        <DivUpload {...getRootProps()}>
+          <IconImage />
+          <input {...getInputProps()} />
+          <p>Arraste uma imagem ou clique aqui</p>
+        </DivUpload>
+      )}
+      <DivOptions>
+        <DivOption>
+          <InputRadio
+            name="option"
+            id="true"
+            value={true}
+            type="radio"
+            onChange={e => {
+              setAnswerCorrect(e.target.value);
+            }}
+            hidden
+          />
+          <LabelOption htmlFor="true">Verdadeiro</LabelOption>
+        </DivOption>
+        <DivOption>
+          <InputRadio
+            name="option"
+            id="false"
+            value={false}
+            type="radio"
+            onChange={e => {
+              setAnswerCorrect(e.target.value);
+            }}
+            hidden
+          />
+          <LabelOption htmlFor="false">Falso</LabelOption>
+        </DivOption>
+      </DivOptions>
+      {error && <TextError>{error}</TextError>}
+      <ButtonCreate onClick={registerQuestion}>Cadastrar questão</ButtonCreate>
+    </Container>
+  );
 }
