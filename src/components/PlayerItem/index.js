6bcc6c41e502f7a8d22f49f78473851d10460318
Vinícios Grama <vinyirun1@gmail.com>
2019-08-06 22:40:29 -0300

    Adiciona imagem de perfil em lista de usuários

--- src/components/PlayerItem/index.js
+++ src/components/PlayerItem/index.js
@@ -1,11 +1,17 @@
 import React from "react";
-import { Container, Nome, DotsIcon } from "./styles";
+import { Container, Nome, ImageProfile, DotsIcon } from "./styles";
+
+import image from "../../assets/img/default-person.png";
 
 const PlayerItem = ({ player, handleClick }) => {
   console.log(player);
   return (
     <Container>
-      <Nome>{player.user.name}</Nome>
+      <div>
+        <ImageProfile src={player.user.path ? player.user.path : image} />
+        <Nome>{player.user.name}</Nome>
+      </div>
+
       <DotsIcon onClick={handleClick} />
     </Container>
   );
--- src/components/PlayerItem/styles.js
+++ src/components/PlayerItem/styles.js
@@ -8,12 +8,25 @@ export const Container = styled.li`
   background-color: #e3e3e3;
   border-radius: 6px;
   align-items: center;
+  margin-bottom: 8px;
+
+  div {
+    display: flex;
+    align-items: center;
+  }
 `;
 
 export const Nome = styled.h3`
   font-family: sans-serif;
   font-weight: 400;
   margin: 2px 0;
+`;
+
+export const ImageProfile = styled.img`
+  height: 36px;
+  width: 36px;
+  border-radius: 20px;
+  margin-right: 10px;
 `;
 
 export const DotsIcon = styled(DotsVerticalRounded)`
