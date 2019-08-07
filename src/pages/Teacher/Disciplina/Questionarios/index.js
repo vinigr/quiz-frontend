97a32d6fad1c77ada6d4222b7f4e1ed66e440e2c
Vinícios Grama <vinyirun1@gmail.com>
2019-08-06 22:43:01 -0300

    Faz o roteamento de páginas de questionários de disciplinas

--- src/pages/Teacher/Disciplina/Questionarios/index.js
+++ src/pages/Teacher/Disciplina/Questionarios/index.js
@@ -1,7 +1,14 @@
 import React from "react";
+import { Switch, Route } from "react-router-dom";
 
-// import { Container } from './styles';
+import List from "./List";
+import NewQuiz from "./NewQuiz";
 
-export default function Questionarios() {
-  return <div />;
+export default function Questionarios(props) {
+  return (
+    <Switch>
+      <Route exact path={`${props.match.path}/`} component={List} />
+      <Route path={`${props.match.path}/new`} component={NewQuiz} />
+    </Switch>
+  );
 }
