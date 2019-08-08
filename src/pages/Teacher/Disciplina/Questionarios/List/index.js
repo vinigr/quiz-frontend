91f0e97ad4b6ad27119ce7ce6bfadba42ebfdec5
Vinícios Grama <vinyirun1@gmail.com>
2019-08-07 21:19:12 -0300

    Inicia página de listagem para quizzes

--- src/pages/Teacher/Disciplina/Questionarios/List/index.js
+++ src/pages/Teacher/Disciplina/Questionarios/List/index.js
@@ -2,13 +2,28 @@ import React, { useState, useEffect } from "react";
 
 import { Container, Add } from "./styles";
 
+import api from "../../../../../service/api";
+
 export default function List(props) {
   useEffect(() => {
     document.title = "Questionários";
-  }, []);
+
+    async function buscaBanco() {
+      try {
+        const listQuiz = await api.get(
+          `/subjectQuizList/${props.match.params.id}`
+        );
+        console.log(listQuiz.data);
+      } catch (error) {
+        console.log(error.response);
+      }
+    }
+    buscaBanco();
+  }, [props.match.params.id]);
 
   return (
     <Container>
+      {/* <p></p> */}
       <Add to={`${props.match.url}/new`}>Adicionar</Add>
     </Container>
   );
