59d192ec2ad48f1bca39559d64dd8edf08f8e384
Vinícios Grama <vinyirun1@gmail.com>
2019-08-07 21:18:50 -0300

    Adiciona funcionalidades para criar quiz

--- src/pages/Teacher/Disciplina/Questionarios/NewQuiz/index.js
+++ src/pages/Teacher/Disciplina/Questionarios/NewQuiz/index.js
@@ -1,16 +1,46 @@
 import React, { useState, useEffect } from "react";
 import DateTime from "@nateradebaugh/react-datetime";
 import "@nateradebaugh/react-datetime/css/react-datetime.css";
+import { makeStyles } from "@material-ui/core/styles";
+import ExpansionPanel from "@material-ui/core/ExpansionPanel";
+import ExpansionPanelSummary from "@material-ui/core/ExpansionPanelSummary";
+import ExpansionPanelDetails from "@material-ui/core/ExpansionPanelDetails";
+import Typography from "@material-ui/core/Typography";
 
 import pt from "date-fns/locale/pt";
 import isAfter from "date-fns/is_after";
 
-import { Container } from "./styles";
+import {
+  Container,
+  DivQuestions,
+  DivImage,
+  Image,
+  IconArrow,
+  DivOptions,
+  DivOption,
+  DivAnswer,
+  Dates
+} from "./styles";
 
-export default function NewQuiz() {
+import api from "../../../../../service/api";
+
+const format = require("date-fns/format");
+
+const useStyles = makeStyles(theme => ({
+  heading: {
+    fontSize: theme.typography.pxToRem(15),
+    fontWeight: theme.typography.fontWeightRegular
+  }
+}));
+
+export default function NewQuiz(props) {
+  const classes = useStyles();
   const [name, setName] = useState("");
-  const [date, setDate] = useState("");
+  const [date, setDate] = useState();
   const [questions, setQuestions] = useState([]);
+  const [selectedQuestionsME, setSelectedQuestionsME] = useState([]);
+  const [selectedQuestionsTF, setSelectedQuestionsTF] = useState([]);
+  const [error, setError] = useState();
 
   function valid(current) {
     return isAfter(current, new Date());
@@ -18,29 +48,242 @@ export default function NewQuiz() {
 
   useEffect(() => {
     document.title = "Novo Quiz";
+
+    async function buscaBanco() {
+      try {
+        const questions = await api.get("/questionsAll");
+        setQuestions(questions.data);
+      } catch (error) {
+        console.log(error);
+      }
+    }
+    buscaBanco();
   }, []);
 
-  // useEffect(() => {
-  //   console.log(typeof date);
-  // }, [date]);
-
   function handleDate(e) {
-    const newDate = new Date(e).toISOString();
-    console.log(e);
-    return setDate(newDate);
+    if (typeof e !== "object") return;
+
+    return setDate(e);
+  }
+
+  function removeQuestionMe(id) {
+    const idQuestions = selectedQuestionsME.filter(question => question !== id);
+    setSelectedQuestionsME(idQuestions);
+  }
+
+  function removeQuestionTf(id) {
+    const idQuestions = selectedQuestionsTF.filter(question => question !== id);
+    setSelectedQuestionsTF(idQuestions);
+  }
+
+  async function registerQuiz() {
+    setError(null);
+    if (!name || name === "") return setError("Dados insuficientes!");
+
+    if (!date || typeof date !== "object") return setError("Data inválida!");
+
+    if (selectedQuestionsME.length + selectedQuestionsTF.length === 0)
+      return setError("As questões não foram selecionadas!");
+
+    try {
+      await api.post("/createQuiz", {
+        name,
+        date,
+        selectedQuestionsME,
+        selectedQuestionsTF,
+        subjectId: props.match.params.id
+      });
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
+  function renderOptions(question) {
+    const arrayOptions = [];
+    arrayOptions.push(
+      question.option1,
+      question.option2,
+      question.option3,
+      question.option4,
+      question.option5
+    );
+    Object.assign(question, { options: arrayOptions });
+
+    const { options } = question;
+
+    return options.map(
+      (option, index) =>
+        option !== null && (
+          <DivOption key={index} position={index} answer={question.answer}>
+            <p>{option}</p>
+          </DivOption>
+        )
+    );
   }
 
   return (
     <Container>
       <h2>Criar quiz</h2>
-      <input type="text" placeholder="Nome" />
-      <DateTime
-        defaultValue={new Date()}
-        locale={pt}
-        dateFormat="DD/MM/YYYY"
-        isValidDate={valid}
-        onChange={e => handleDate(e)}
+      <input
+        type="text"
+        placeholder="Nome"
+        value={name}
+        onChange={e => setName(e.target.value)}
       />
+      <label>
+        Data de liberação:
+        <DateTime
+          defaultValue={new Date()}
+          value={date}
+          locale={pt}
+          dateFormat="DD/MM/YYYY"
+          isValidDate={valid}
+          onChange={e => handleDate(e)}
+          placeholder="Data de liberação"
+        />
+      </label>
+      <h3>Questões</h3>
+      <span>
+        {selectedQuestionsME.length + selectedQuestionsTF.length !== 0 &&
+          `${selectedQuestionsME.length +
+            selectedQuestionsTF.length} selecionada(s)`}
+      </span>
+      <DivQuestions>
+        <h4>Múltipla escolha</h4>
+        {questions.questionsMe &&
+          questions.questionsMe.map(question => (
+            <ExpansionPanel key={question.id}>
+              <ExpansionPanelSummary
+                style={{
+                  backgroundColor:
+                    selectedQuestionsME.indexOf(question.id) !== -1 && "#37996b"
+                }}
+                expandIcon={<IconArrow />}
+                aria-controls="panel1a-content"
+                id="panel1a-header"
+              >
+                <Typography className={classes.heading}>
+                  {question.question}
+                </Typography>
+              </ExpansionPanelSummary>
+              <ExpansionPanelDetails
+                style={{ display: "flex", flexDirection: "column" }}
+              >
+                {question.pathImage && (
+                  <DivImage>
+                    <Image src={question.pathImage} />
+                  </DivImage>
+                )}
+                <DivOptions>{renderOptions(question)}</DivOptions>
+                <Dates>
+                  <div>
+                    <span>Data de criação</span>
+                    <span>{format(question.createdAt, "DD/MM/YYYY")}</span>
+                  </div>
+                  <div>
+                    <span>Data de atualização</span>
+                    <span>{format(question.updatedAt, "DD/MM/YYYY")}</span>
+                  </div>
+                </Dates>
+                <div className="button">
+                  {selectedQuestionsME.indexOf(question.id) !== -1 ? (
+                    <button
+                      className="remove"
+                      onClick={() => {
+                        removeQuestionMe(question.id);
+                      }}
+                    >
+                      Remover
+                    </button>
+                  ) : (
+                    <button
+                      className="add"
+                      onClick={async () => {
+                        setSelectedQuestionsME([
+                          ...selectedQuestionsME,
+                          question.id
+                        ]);
+                      }}
+                    >
+                      Adicionar
+                    </button>
+                  )}
+                </div>
+              </ExpansionPanelDetails>
+            </ExpansionPanel>
+          ))}
+        <h4>Verdadeiro e falso</h4>
+        {questions.questionsTf &&
+          questions.questionsTf.map(question => (
+            <ExpansionPanel key={question.id}>
+              <ExpansionPanelSummary
+                style={{
+                  backgroundColor:
+                    selectedQuestionsTF.indexOf(question.id) !== -1 && "#37996b"
+                }}
+                expandIcon={<IconArrow />}
+                aria-controls="panel1a-content"
+                id="panel1a-header"
+              >
+                <Typography className={classes.heading}>
+                  {question.question}
+                </Typography>
+              </ExpansionPanelSummary>
+              <ExpansionPanelDetails
+                style={{ display: "flex", flexDirection: "column" }}
+              >
+                {question.pathImage && (
+                  <DivImage>
+                    <Image src={question.pathImage} />
+                  </DivImage>
+                )}
+                <DivOptions>
+                  <DivAnswer answer={question.answer}>
+                    <p>{question.answer ? "Verdadeiro" : "Falso"}</p>
+                  </DivAnswer>
+                </DivOptions>
+                <Dates>
+                  <div>
+                    <span>Data de criação</span>
+                    <span>{format(question.createdAt, "DD/MM/YYYY")}</span>
+                  </div>
+                  <div>
+                    <span>Data de atualização</span>
+                    <span>{format(question.updatedAt, "DD/MM/YYYY")}</span>
+                  </div>
+                </Dates>
+                <div className="button">
+                  {selectedQuestionsTF.indexOf(question.id) !== -1 ? (
+                    <button
+                      className="remove"
+                      onClick={() => {
+                        removeQuestionTf(question.id);
+                      }}
+                    >
+                      Remover
+                    </button>
+                  ) : (
+                    <button
+                      className="add"
+                      onClick={async () => {
+                        setSelectedQuestionsTF([
+                          ...selectedQuestionsTF,
+                          question.id
+                        ]);
+                      }}
+                    >
+                      Adicionar
+                    </button>
+                  )}
+                </div>
+              </ExpansionPanelDetails>
+            </ExpansionPanel>
+          ))}
+      </DivQuestions>
+      {error && <h4 className="error">{error}</h4>}
+      <button className="register" onClick={registerQuiz}>
+        Cadastrar quiz
+      </button>
     </Container>
   );
 }
--- src/pages/Teacher/Disciplina/Questionarios/NewQuiz/styles.js
+++ src/pages/Teacher/Disciplina/Questionarios/NewQuiz/styles.js
@@ -1,4 +1,5 @@
 import styled from "styled-components";
+import { KeyboardArrowDown } from "styled-icons/material/KeyboardArrowDown";
 
 export const Container = styled.div`
   display: flex;
@@ -8,9 +9,10 @@ export const Container = styled.div`
 
   h2 {
     font-size: 24px;
+    margin-bottom: 10px;
   }
 
-  input[type="text"] {
+  input {
     height: 40px;
     border: none;
     border-radius: 3px;
@@ -19,4 +21,147 @@ export const Container = styled.div`
     width: 400px;
     padding: 0 5px;
   }
+
+  label {
+    display: flex;
+    flex-direction: column;
+    font-weight: 500;
+    font-size: 14px;
+  }
+
+  h4 {
+    margin: 10px 0;
+  }
+
+  span {
+    font-weight: 500;
+    color: #616a6b;
+    margin: 5px 0;
+  }
+
+  h4.error {
+    font-weight: 500;
+    font-size: 16px;
+    text-align: center;
+    color: #d41919;
+    margin: 10px 0 6px;
+  }
+
+  button {
+    padding: 10px;
+    border-radius: 4px;
+    border: none;
+    cursor: pointer;
+  }
+
+  button.register {
+    margin-top: 20px;
+  }
+
+  button:hover {
+    opacity: 0.8;
+  }
+`;
+
+export const DivQuestions = styled.div`
+  width: 80%;
+
+  .button {
+    display: flex;
+    width: 100%;
+    justify-content: flex-end;
+  }
+
+  button {
+    width: 80px;
+    height: 50px;
+    font-weight: 500;
+    border: none;
+    border-radius: 4px;
+    cursor: pointer;
+    color: #fff;
+  }
+
+  button.add {
+    background-color: #37996b;
+  }
+
+  button.remove {
+    background-color: #cf4848;
+  }
+`;
+
+export const DivOptions = styled.div`
+  width: 80%;
+`;
+
+export const DivOption = styled.div`
+  padding: 10px;
+  margin: 10px 0;
+  border-radius: 4px;
+  background-color: ${props =>
+    props.answer === props.position ? "#37996b" : "#DDDDDD"};
+
+  p {
+    font-weight: 400;
+  }
+`;
+
+export const DivAnswer = styled.div`
+  padding: 10px;
+  margin: 10px 0;
+  border-radius: 4px;
+  background-color: ${props => (props.answer ? "#37996b" : "#CF4848")};
+  width: 100px;
+  display: flex;
+  justify-content: center;
+  p {
+    font-weight: 500;
+    color: #fff;
+    text-align: center;
+  }
+`;
+
+export const Dates = styled.div`
+  width: 100%;
+  display: flex;
+  justify-content: space-between;
+  margin: 20px 0 20px;
+
+  div {
+    display: flex;
+    flex-direction: column;
+    font-weight: 500;
+    color: #4d4d4d;
+    font-size: 10px;
+  }
+
+  div:last-child {
+    span {
+      text-align: end;
+    }
+  }
+`;
+
+export const DivImage = styled.div`
+  border-radius: 4px;
+  padding: 20px;
+  text-align: center;
+  border: 1px dashed #ddd;
+  color: #999;
+  margin-top: 20px;
+  cursor: pointer;
+  width: 60%;
+  margin-bottom: 20px;
+  display: flex;
+  flex-direction: column;
+`;
+
+export const Image = styled.img`
+  width: 100%;
+`;
+
+export const IconArrow = styled(KeyboardArrowDown)`
+  color: #b7b7b7;
+  width: 24px;
 `;
