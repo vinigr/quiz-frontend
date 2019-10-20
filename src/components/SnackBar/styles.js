b8ff2bea097a8e70a87a328c711fb99bfe0a137c
Vinícios Grama <vinyirun1@gmail.com>
2019-10-19 23:43:19 -0300

    :sparkles: Cria SnackBar para notificações

---
+++ src/components/SnackBar/styles.js
@@ -0,0 +1,16 @@
+import styled from "styled-components";
+import { CheckCircle } from "styled-icons/material/CheckCircle";
+import { Close } from "styled-icons/material/Close";
+import { Error } from "styled-icons/material/Error";
+
+export const CheckCircleIcon = styled(CheckCircle)`
+  width: 20px;
+`;
+
+export const CloseIcon = styled(Close)`
+  width: 20px;
+`;
+
+export const ErrorIcon = styled(Error)`
+  width: 20px;
+`;
