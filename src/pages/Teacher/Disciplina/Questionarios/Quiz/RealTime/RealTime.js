8ffba4d37e4e82692f5b5bc4f52fab11b4643f54
Vinícios Grama <vinyirun1@gmail.com>
2019-11-04 12:39:12 -0300

    Cria tela para observação dos resultados em tempo real

---
+++ src/pages/Teacher/Disciplina/Questionarios/Quiz/RealTime/RealTime.js
@@ -0,0 +1,36 @@
+import React, { useState, useEffect } from "react";
+
+import { Container } from "./styles";
+
+import RankingQuiz from "../../../../../../components/RankingQuiz";
+
+import api from "../../../../../../service/api";
+
+const RealTime = props => {
+  const [quiz, setQuiz] = useState();
+  const [questions, setQuestions] = useState();
+
+  useEffect(() => {
+    async function fetchData() {
+      try {
+        const quizId = props.match.params.id;
+        const { data } = await api.get(`/infoQuiz/${quizId}`);
+        setQuiz(data.quiz.name);
+        setQuestions(data.questions);
+      } catch (error) {
+        console.log(error.response.data);
+      }
+    }
+
+    fetchData();
+  }, [props.match.params, props.match.params.id]);
+
+  return (
+    <Container>
+      <h2>{quiz}</h2>
+      <RankingQuiz name={quiz} total={questions} {...props} />
+    </Container>
+  );
+};
+
+export default RealTime;
---
+++ src/pages/Teacher/Disciplina/Questionarios/Quiz/RealTime/styles.js
@@ -0,0 +1,29 @@
+import styled from "styled-components";
+
+export const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  width: 100%;
+
+  h2 {
+    font-family: "Rubik";
+  }
+
+  button#real-time {
+    border-radius: 2px;
+    border: none;
+    padding: 6px;
+    margin-bottom: 10px;
+
+    &:hover {
+      cursor: pointer;
+    }
+  }
+
+  div.hits {
+    display: flex;
+    flex-direction: column;
+    align-items: flex-end;
+  }
+`;
