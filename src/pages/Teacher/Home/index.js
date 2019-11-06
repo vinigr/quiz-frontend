cc422a9f58bb889e97a36568f7fa7e8510845683
Vinícios Grama <vinyirun1@gmail.com>
2019-11-06 13:56:44 -0300

    :construction: Inicia tela de estatísticas gerais

--- src/pages/Teacher/Home/index.js
+++ src/pages/Teacher/Home/index.js
@@ -1,10 +1,86 @@
-import React from "react";
-// import { Container } from "./styles";
+import React, { useState, useEffect } from "react";
+import {
+  BarChart,
+  Bar,
+  XAxis,
+  YAxis,
+  CartesianGrid,
+  Tooltip,
+  LineChart,
+  Line,
+  PieChart,
+  Pie,
+  Cell
+} from "recharts";
 
-export default function Home() {
+import api from "../../../service/api";
+import { Container } from "./styles";
+
+export default function Home(props) {
+  const [tops, setTops] = useState([]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const { data } = await api.get(`/teacher/statistics`);
+
+      const listTops = data.tops.map(user => {
+        return {
+          Pontuacão: user.scoreAll,
+          name: user.User.name
+        };
+      });
+
+      // const listAverage = data.averageQuizzes.map(quiz => {
+      //   return {
+      //     name: quiz.Quiz.name,
+      //     Pontuação: parseFloat(quiz.average)
+      //   };
+      // });
+
+      // const answersResult = [
+      //   { name: "Acerto", value: data.answersStats.hit },
+      //   { name: "Erro", value: data.answersStats.error },
+      //   { name: "Pulo", value: data.answersStats.skip }
+      // ];
+
+      await setTops(listTops);
+
+      // await setAverage(listAverage);
+
+      // await setAnswers(answersResult);
+    };
+
+    fetchData();
+  }, [props.match.params]);
   return (
-    <>
-      <h1>Olá</h1>
-    </>
+    <Container>
+      <div className="graphics">
+        {tops.length > 0 ? (
+          <>
+            <h2>Melhores pontuadores</h2>
+
+            <BarChart
+              width={500}
+              height={300}
+              data={tops}
+              margin={{
+                top: 5,
+                right: 10,
+                left: 10,
+                bottom: 5
+              }}
+            >
+              <CartesianGrid strokeDasharray="3 3" />
+              <XAxis dataKey="name" />
+              <YAxis />
+              <Tooltip />
+              <Bar dataKey="Pontuacão" fill="#8FC52E" />
+            </BarChart>
+          </>
+        ) : (
+          <h2>Ainda sem estatísticas</h2>
+        )}
+      </div>
+    </Container>
   );
 }
---
+++ src/pages/Teacher/Home/styles.js
@@ -0,0 +1,20 @@
+import styled from "styled-components";
+
+export const Container = styled.div`
+  display: flex;
+  font-family: "Rubik", sans-serif;
+  flex-wrap: wrap;
+  justify-content: center;
+  padding: 20px;
+
+  .graphics {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+
+    h2 {
+      font-size: 14px;
+      margin-bottom: 10px;
+    }
+  }
+`;
