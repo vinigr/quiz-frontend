f4548eb50dbbc8faf06913bc0c81fcbf7f3addcb
Vinícios Grama <vinyirun1@gmail.com>
2019-11-08 16:41:15 -0300

    :construction: Inicia tela sobre informações do projeto

---
+++ src/pages/Teacher/Sobre/Sobre.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+// import { Container } from './styles';
+
+export default function Sobre() {
+  return (
+    <div>
+      <p>
+        Plataforma desenvolvida na disciplina de "TID - Trabalho
+        Interdisciplinar Dirigido" pelos alunos.
+      </p>
+    </div>
+  );
+}
